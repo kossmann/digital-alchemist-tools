8d590b4de833104f8116d8d6c56efe33117e07c5
Daniel Kossmann Ferraz <daniel@spirallab.com.br>
2017-08-20 19:48:30 -0300

    TimeSum: Input is now interactive

--- timesum.py
+++ timesum.py
@@ -2,30 +2,40 @@
 # -*- coding: utf-8 -*-
 import sys
 import datetime
+import readline
 
-# usage: python timesum.py 10:45 00:50
-
-timeList = map(str,sys.argv[1:])
+readline.parse_and_bind('tab: complete')
+readline.parse_and_bind('set editing-mode vi')
 
 sum = datetime.timedelta()
 
-for i in timeList:
-    if ( i.count(":") + 1 == 1 ):
-        h = i
-        m = '00'
-        s = '00'
-    elif ( i.count(":") + 1 == 2 ):
-        (h, m) = i.split(':')
-        s = '00'
-    elif ( i.count(":") + 1 == 3 ):
-        (h, m, s) = i.split(':')
-    else:
-        h = '00'
-        m = '00'
-        s = '00'
-        
-    d = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
-    
-    sum += d
+print 'Type hr(:min(:sec)) ("q" to quit): '
 
-print(str(sum))
+while True:
+	line = raw_input('')
+    
+	if line == 'q':
+		break
+
+	timeList = map(str,sys.argv[1:])
+
+	# Format input
+	if ( line.count(":") + 1 == 1 ):
+		h = line
+		m = '00'
+		s = '00'
+	elif ( line.count(":") + 1 == 2 ):
+		(h, m) = line.split(':')
+		s = '00'
+	elif ( line.count(":") + 1 == 3 ):
+		(h, m, s) = line.split(':')
+	else:
+		h = '00'
+		m = '00'
+		s = '00'
+			
+	hour = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+		
+	sum += hour
+
+	print("Sum = " + str(sum))
